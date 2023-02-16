27867298fbd94ff04f198179323ff66aec36e7e8
Alemzewd Belay <112337400+Kirubelay@users.noreply.github.com>
2023-02-16 13:55:03 -0800

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,13 @@
-/*
- * File: 4-puts.c
- */
-
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
- * 
- * Return: Always 0.
- */
+  *main - print progrmmming is like building
+  *
+  *
+  *Return: Always 0
+  **/
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
