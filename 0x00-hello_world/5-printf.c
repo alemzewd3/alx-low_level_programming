4bb200946ff6ec237af3f42f22203bd8ba11292c
Alemzewd Belay <112337400+Kirubelay@users.noreply.github.com>
2023-02-16 13:57:53 -0800

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,12 @@
-/*
- * File: 6-size.c
- * Auth: alemzewd
- */
-
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- * main - Prints the size of various types based on
- *       the computer it is compiled and run on..
+ *main - printing
  *
- * Return: Always 0.
- */
+ *Return: 0
+ **/
 int main(void)
 {
-		printf("Size of a char: %zu byte(s)\n", sizeof(char));
-		printf("Size of an int: %zu byte(s)\n", sizeof(int));
-		printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-		printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-		printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
