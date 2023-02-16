74fe4cc043a0d04161aef33dcb61c273e4967089
Alemzewd Belay <112337400+Kirubelay@users.noreply.github.com>
2023-02-16 15:09:08 -0800

    Create 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  *main - printing file
- * 
+ *
  *Return: 1
- */
+ **/
 int main(void)
 {
-		printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-		return (1);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
