a8dcf0e4ae9c72646f61895205827ca3d05b1a69
kirubelay <kirubelay>
2023-02-16 13:40:54 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,18 @@
 /*
- * 4-puts.c
+ * File: 4-puts.c
  */
-#include<stdio.h>
+
+#include <stdio.h>
+
 /**
- * main - prints "Programming is like building a multiplingual puzzle "
- * Return : always 0.
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ * 
+ * Return: Always 0.
  */
 int main(void)
 {
-	puts("\"Programmig is like building a multiplingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 }
