239c27aaf8a6d4fc9e0603655e1b2709561be4cd
kirubelay <kirubelay>
2023-02-16 14:59:04 -0800

    final

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+nclude<stdio.h>
+
+/**
+ *main - printing file
+ * 
+ *Return: 1
+ */
+int main(void)
+{
+		printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		return (1);
+}
