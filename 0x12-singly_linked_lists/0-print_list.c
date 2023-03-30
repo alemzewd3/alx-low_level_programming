c284f6aa1b590198bccf9b574f96c77dc274fca0
kirubelay <kirubelay6@gmail.com>
2023-03-30 12:17:02 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_list - prints all the elements of a linked list
- * @h: pointer to the list_t list to print
+ * print_list - Prints all the elements of a list_t list.
+ * @s: The list_t list.
  *
- * Return: the number of nodes printed
+ * Return: The number of nodes in s.
  */
-size_t print_list(const list_t *h)
+
+size_t print_list(const list_t *s)
 {
- size_t s = 0;
+	size_t nodes = 0;
 
- while (h)
- {
- if (!h->str)
- printf("[0] (nil)\n");
- else
- printf("[%u] %s\n", h->len, h->str);
- h = h->next;
- s++;
- }
-
- return (s);
+	while (s != NULL)
+	{
+		if (s->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", s->len, s->str);
+		s = s->next;
+		nodes++;
+	}
+	return (nodes);
 }
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,20 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
- *
- * Return: number of elements in h
+ * list_len - returns then number of elements in a list.
+ * @s: singly linked list.
+ * Return: number of elements in the list.
  */
-size_t list_len(const list_t *h)
-{
- size_t n = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+size_t list_len(const list_t *s)
+{
+	size_t nodes;
+
+	for (nodes = 0 ; s != NULL ; nodes++)
+	{
+		s = s->next;
+	}
+
+	return (nodes);
 }
